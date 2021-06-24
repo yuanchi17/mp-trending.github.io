d7c62a5e0d09a5816d109be0d7badbc689577151
taichunmin <taichunmin@gmail.com>
2021-06-24 20:09:54 +0800

    捷安特旅行社210630

---
+++ build.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+const _ = require('lodash')
+const { errToPlainObj, getBaseurl, getenv, ncp } = require('./utils')
+const { inspect } = require('util')
+const fg = require('fast-glob')
+const fsPromises = require('fs').promises
+const htmlMinifier = require('html-minifier').minify
+const log = require('debug')('app:index')
+const path = require('path')
+const pug = require('pug')
+const UglifyJS = require('uglify-js')
+
+exports.build = async () => {
+  const PUG_OPTIONS = {
+    basedir: path.resolve(__dirname),
+    baseurl: getBaseurl(),
+    GA_MEASUREMENT_ID: getenv('GA_MEASUREMENT_ID', 'G-SYT6P9ENET'),
+    NODE_ENV: getenv('NODE_ENV', 'production'),
+    ..._.fromPairs(_.map([
+      'LIFFID_FULL',
+    ], k => [_.camelCase(k), getenv(k)])),
+  }
+
+  const htmlMinifierOptions = {
+    caseSensitive: true,
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    decodeEntities: true,
+    minifyCSS: true,
+    minifyJS: code => UglifyJS.minify(code).code,
+    removeCDATASectionsFromCDATA: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+  }
+
+  // copy public files
+  await ncp('src', 'dist', {
+    stopOnErr: true,
+    filter: filename => !/\.pug$/.test(filename), // 複製除了 .pug 之外的檔案
+  })
+
+  // compile pug files
+  const pugFiles = _.map(await fg('src/**/*.pug'), file => file.slice(4))
+
+  let pugErrors = 0
+  for (const file of pugFiles) {
+    try {
+      const html = htmlMinifier(pug.renderFile(path.resolve(__dirname, 'src', file), PUG_OPTIONS), htmlMinifierOptions)
+      const dist = path.resolve(__dirname, 'dist', file.replace(/\.pug$/, '.html'))
+      await fsPromises.mkdir(path.dirname(dist), { recursive: true })
+      await fsPromises.writeFile(dist, html)
+    } catch (err) {
+      _.set(err, 'data.src', `./src/${file}`)
+      log(`Failed to render pug, err = ${inspect(errToPlainObj(err), { depth: 100, sorted: true })}`)
+      pugErrors++
+    }
+  }
+  if (pugErrors) throw new Error(`Failed to render ${pugErrors} pug files.`)
+}
